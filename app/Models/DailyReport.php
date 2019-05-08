cc85cc3ed34aa00849e111c14e49fbe475391485
tyanagstr <ph21.heinriches12@gmail.com>
2019-05-08 18:06:44 +0900

    日報モデルとコントローラを追加

--- app/Models/DailyReport.php
+++ app/Models/DailyReport.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class DailyReport extends Model
 {
-    //
+    protected $fillable = [ 
+        'title',
+        'contents',
+        'reporting_time'
+    ];
 }
