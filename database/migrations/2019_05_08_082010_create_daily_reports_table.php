31961444c598ca87d42291f43b539e6eb9e888bd
tyanagstr <ph21.heinriches12@gmail.com>
2019-05-08 17:29:31 +0900

    日報dbのmigrationファイルを作成

---
+++ database/migrations/2019_05_08_082010_create_daily_reports_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDailyReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('daily_reports', function (Blueprint $table) {
+            $table->increments('user_id');      // 日報の投稿者
+            $table->string('title');            // 日報のタイトル
+            $table->text('contents');           // 日報の内容
+            $table->datetime('reporting_time'); // 日報の日付
+            $table->timestamps();               // created_at / updated_at 
+                                                // 作成日時 / 更新日時
+            $table->timestamp('deleted_at');    // deleted_at 削除日時
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('daily_reports', function (Blueprint $table) {
+            $table->dropIfExists();
+        });
+    }
+}
