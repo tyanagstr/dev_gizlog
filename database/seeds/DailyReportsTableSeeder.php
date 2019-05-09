0b84ed53066ee19861bfb0440161414aefb936d0
tyanagstr <ph21.heinriches12@gmail.com>
2019-05-09 10:12:58 +0900

    日報のSeederに手書きのレコード生成

--- database/seeds/DailyReportsTableSeeder.php
+++ database/seeds/DailyReportsTableSeeder.php
@@ -14,10 +14,21 @@ class DailyReportsTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('daily_reports')->truncate();
+        $tableName = 'daily_reports';
+        DB::table($tableName)->truncate();
+        // 手書きのテスト用シード
+        DB::table($tableName)->insert([
+            'user_id' => 1,
+            'title' => '日報テスト',
+            'contents' => 'これは日報です',
+            'reporting_time' => Carbon::now(),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        // 自動生成シード
         $faker = Faker::create('en_US');
         for ($i = 0; $i < 10; $i++) { 
-            DB::table('daily_reports')->insert([
+            DB::table($tableName)->insert([
                 'user_id' => $i,
                 'title' => $faker->sentence(),
                 'contents' => $faker->text(),
