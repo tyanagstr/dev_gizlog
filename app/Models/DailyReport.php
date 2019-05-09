5ee989c757c9aff277d02d5d46bbaffee124c57a
tyanagstr <ph21.heinriches12@gmail.com>
2019-05-09 12:15:32 +0900

    日報テーブルのdeleted_atをクエリビルダのsoftDeletesを使用したものに変更

--- app/Models/DailyReport.php
+++ app/Models/DailyReport.php
@@ -3,11 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class DailyReport extends Model
 {
+    use SoftDeletes;
+
     // 取得時Carbonでラップするカラムを指定
-    protected $dates = ['reporting_time'];   
+    protected $dates = ['reporting_time', 'deleted_at'];   
 
     // fillでモデルにセットするカラムを指定
     protected $fillable = [ 
--- database/migrations/2019_05_08_082010_create_daily_reports_table.php
+++ database/migrations/2019_05_08_082010_create_daily_reports_table.php
@@ -15,14 +15,13 @@ class CreateDailyReportsTable extends Migration
     {
         Schema::create('daily_reports', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id');      // 日報の投稿者
+            $table->integer('user_id');         // 日報の投稿者
             $table->string('title');            // 日報のタイトル
             $table->text('contents');           // 日報の内容
             $table->datetime('reporting_time'); // 日報の日付
             $table->timestamps();               // created_at / updated_at 
                                                 // 作成日時 / 更新日時
-            $table->timestamp('deleted_at')
-                  ->nullable();                 // 削除日時
+            $table->softDeletes();              // deleted_at 削除日時
         });
     }
 
