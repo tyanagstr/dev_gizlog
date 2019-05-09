24e89d34534ba4d3f802e76467cc4477aba3d022
tyanagstr <ph21.heinriches12@gmail.com>
2019-05-09 12:45:02 +0900

    formの階層div.container下に変更

--- resources/views/user/daily_report/create.blade.php
+++ resources/views/user/daily_report/create.blade.php
@@ -3,27 +3,29 @@
 
 <h2 class="brand-header">日報作成</h2>
 <div class="main-wrap">
-  {!! Form::open(['route' => 'daily_report.store', 'class' => 'container form']) !!}
-    {!! Form::hidden('user_id', Auth::user()->id, ['class' => 'form-control']) !!}
-    <div class="form-group form-size-small {{ $errors->has('reporting_time')? 'has-error' : ''}}">
-      {!! Form::date('reporting_time', null, ['class' => 'form-control']) !!}
-      <span class="help-block">{{ $errors->first('reporting_time') }}</span>
-    </div>
-    <div class="form-group {{ $errors->has('title')? 'has-error' : ''}}">
-      {!! Form::text('title', '', ['class' => 'form-control', 'placeholder' => 'Title']) !!}
-      <span class="help-block">{{ $errors->first('title') }}</span>
-    </div>
-    <div class="form-group {{ $errors->has('contents')? 'has-error' : ''}}">
-      {!! Form::textarea('contents', '', [
-        'class' => 'form-control',
-        'placeholder' => 'Content',
-        'cols' => '50',
-        'rows' => '10'
-      ]) !!}
-      <span class="help-block">{{ $errors->first('contents') }}</span>
-    </div>
-    {!! Form::submit('Add', ['class' => 'btn btn-success pull-right']) !!}
-  {!! Form::close() !!}
+  <div class="container">
+    {!! Form::open(['route' => 'daily_report.store']) !!}
+      {!! Form::hidden('user_id', Auth::user()->id, ['class' => 'form-control']) !!}
+      <div class="form-group form-size-small {{ $errors->has('reporting_time')? 'has-error' : ''}}">
+        {!! Form::date('reporting_time', null, ['class' => 'form-control']) !!}
+        <span class="help-block">{{ $errors->first('reporting_time') }}</span>
+      </div>
+      <div class="form-group {{ $errors->has('title')? 'has-error' : ''}}">
+        {!! Form::text('title', '', ['class' => 'form-control', 'placeholder' => 'Title']) !!}
+        <span class="help-block">{{ $errors->first('title') }}</span>
+      </div>
+      <div class="form-group {{ $errors->has('contents')? 'has-error' : ''}}">
+        {!! Form::textarea('contents', '', [
+          'class' => 'form-control',
+          'placeholder' => 'Content',
+          'cols' => '50',
+          'rows' => '10'
+        ]) !!}
+        <span class="help-block">{{ $errors->first('contents') }}</span>
+      </div>
+      {!! Form::submit('Add', ['class' => 'btn btn-success pull-right']) !!}
+    {!! Form::close() !!}
+  </div>
 </div>
 
 @endsection
