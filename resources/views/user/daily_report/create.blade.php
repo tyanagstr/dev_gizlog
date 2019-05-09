8cec7634984af35c7d9dead1099965424a4db2d7
tyanagstr <ph21.heinriches12@gmail.com>
2019-05-09 11:36:50 +0900

    バリデーションエラー時のエラークラスを付与

--- resources/views/user/daily_report/create.blade.php
+++ resources/views/user/daily_report/create.blade.php
@@ -5,15 +5,15 @@
 <div class="main-wrap">
   {!! Form::open(['route' => 'daily_report.store', 'class' => 'container form']) !!}
     {!! Form::hidden('user_id', Auth::user()->id, ['class' => 'form-control']) !!}
-    <div class="form-group form-size-small">
+    <div class="form-group form-size-small {{ $errors->has('reporting_time')? 'has-error' : ''}}">
       {!! Form::date('reporting_time', null, ['class' => 'form-control']) !!}
       <span class="help-block">{{ $errors->first('reporting_time') }}</span>
     </div>
-    <div class="form-group">
+    <div class="form-group {{ $errors->has('title')? 'has-error' : ''}}">
       {!! Form::text('title', '', ['class' => 'form-control', 'placeholder' => 'Title']) !!}
       <span class="help-block">{{ $errors->first('title') }}</span>
     </div>
-    <div class="form-group">
+    <div class="form-group {{ $errors->has('contents')? 'has-error' : ''}}">
       {!! Form::textarea('contents', '', [
         'class' => 'form-control',
         'placeholder' => 'Content',
