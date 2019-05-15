d3f5729c8a2d768f91f42f04b9be8a32d96874fa
tyanagstr <ph21.heinriches12@gmail.com>
2019-05-15 14:50:50 +0900

    質問モデルの設定

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -3,9 +3,31 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Question extends Model
 {
-    //
-}
+    use SoftDeletes;
 
+    protected $dates = [
+        'updated_at',
+        'deleted_at'
+    ];
+    
+    protected $fillable = [
+        'user_id',
+        'tag_category_id',
+        'title',
+        'content'
+    ];
+    
+    public function user()
+    {
+        $this->belongsTo(User::class);
+    }
+
+    public function tagCategory()
+    {
+        $this->belongsTo(TagCategory::class);
+    }
+}
