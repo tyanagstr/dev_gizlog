b5b8ce2c345429fa0f302b5fd7af7839cc2118e8
tyanagstr <ph21.heinriches12@gmail.com>
2019-05-15 15:06:22 +0900

    コメントモデルの設定

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -3,8 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Comment extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $dates = [
+        'created_at',
+        'deleted_at'
+    ];
+
+    protected $fillable = [
+        'user_id',
+        'question_id',
+        'comment',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
+    }
 }
