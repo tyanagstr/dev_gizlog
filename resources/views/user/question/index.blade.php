e8d6ad075143a07706469d7e9c94fb18078a1df4
tyanagstr <ph21.heinriches12@gmail.com>
2019-05-16 10:43:31 +0900

    全カテゴリの質問一覧画面を作成

--- resources/views/user/question/index.blade.php
+++ resources/views/user/question/index.blade.php
@@ -17,7 +17,7 @@
     <div class="category-wrap">
       <div class="btn all" id="0">all</div>
       @foreach ($categories as $category)
-    <div class="btn {{ $category->name }}" id="{{ $category->id }}">{{ $category->name }}</div>
+        <div class="btn {{ $category->name }}" id="{{ $category->id }}">{{ $category->name }}</div>
       @endforeach
       <input id="category-val" name="tag_category_id" type="hidden" value="">
     </div>
@@ -34,17 +34,19 @@
         </tr>
       </thead>
       <tbody>
-        <tr class="row">
-          <td class="col-xs-1"><img src="" class="avatar-img"></td>
-          <td class="col-xs-2"></td>
-          <td class="col-xs-6"></td>
-          <td class="col-xs-1"><span class="point-color"></span></td>
-          <td class="col-xs-2">
-            <a class="btn btn-success" href="">
-              <i class="fa fa-comments-o" aria-hidden="true"></i>
-            </a>
-          </td>
-        </tr>
+        @foreach ($questions as $question)
+          <tr class="row">
+          <td class="col-xs-1"><img src="{{ $question->user->avater }}" class="avatar-img">{{ $question->user->name }}</td>
+          <td class="col-xs-2">{{ $question->tagCategory->name }}</td>
+          <td class="col-xs-6">{{ $question->title }}</td>
+          <td class="col-xs-1">{{ $question->comments->count() }}<span class="point-color"></span></td>
+            <td class="col-xs-2">
+              <a class="btn btn-success" href="">
+                <i class="fa fa-comments-o" aria-hidden="true"></i>
+              </a>
+            </td>
+          </tr>
+        @endforeach
       </tbody>
     </table>
     <div aria-label="Page navigation example" class="text-center"></div>
