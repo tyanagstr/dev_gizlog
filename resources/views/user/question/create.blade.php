d5439bd4811788ea6f2432276eaf564a562a802f
tyanagstr <ph21.heinriches12@gmail.com>
2019-05-16 16:06:42 +0900

    質問作成画面のエラー時のクラスを適用する要素を修正

--- resources/views/user/question/create.blade.php
+++ resources/views/user/question/create.blade.php
@@ -5,25 +5,25 @@
 <div class="main-wrap">
   <div class="container">
     {!! Form::open(['route' => 'question.store', 'method' => 'post']) !!}
-      <div class="form-group">
+      <div class="form-group {{ $errors->has('tag_category_id') ? 'has-error': '' }}">
         {!! Form::hidden('user_id', Auth::id()) !!}
         {!! Form::select('tag_category_id', $categories, old('categories'), [
           'placeholder' => 'Select category',
           'class' => "form-control selectpicker form-size-small",
           'id' => 'pref_id']) !!}
-        <span class="help-block {{ $errors->has('tag_category_id') ? 'has-error': '' }}">{{ $errors->first('tag_category_id') }}</span>
+        <span class="help-block">{{ $errors->first('tag_category_id') }}</span>
       </div>
-      <div class="form-group">
+      <div class="form-group {{ $errors->has('title') ? 'has-error': '' }}">
         {!! Form::text('title', old('title'), ['class' => 'form-control', 'placeholder' => 'title']) !!}
-        <span class="help-block {{ $errors->has('title') ? 'has-error': '' }}">{{ $errors->first('title') }}</span>
+        <span class="help-block">{{ $errors->first('title') }}</span>
       </div>
-      <div class="form-group">
+      <div class="form-group {{ $errors->has('content') ? 'has-error': '' }}">
         {!! Form::textarea('content', old('content'), [
           'class' => 'form-control', 
           'placeholder' => 'Please write down your question here...',
           'cols' => '50',
           'rows' => '10']) !!}
-        <span class="help-block {{ $errors->has('content') ? 'has-error': '' }}">{{ $errors->first('content') }}</span>
+        <span class="help-block">{{ $errors->first('content') }}</span>
       </div>
       {!! Form::submit('create', ['name' => 'confirm', 'class' => 'btn btn-success pull-right']) !!}
     {!! Form::close() !!}
